92c50d47b10edd6476c5ed6138a438650c359f61
kanjitta <jittakan2539@gmail.com>
2024-05-29 12:38:51 +0700

    finish exercise 4

--- exercise_04/App.jsx
+++ exercise_04/App.jsx
@@ -1,12 +1,55 @@
 import React, { useState } from "react";
 
 const App = () => {
-	const [temperature, setTemperature] = useState(25); // Initial temperature value
+	const [temperature, setTemperature] = useState(25); //Initial temperature value
 
 	const decreaseTemperature = () => setTemperature((prevTemp) => prevTemp - 1);
 	const increaseTemperature = () => setTemperature((prevTemp) => prevTemp + 1);
-
-	return <>{/* code here */};</>;
+	return (
+		<>
+			<Header temperature={temperature} />;
+			<Content temperature={temperature} />;
+			<Footer
+				decreaseTemperature={decreaseTemperature}
+				increaseTemperature={increaseTemperature}
+			/>
+		</>
+	);
 };
 
+const Header = ({ temperature }) => {
+	return (
+		<div>
+			<button>ON / OFF</button>
+			<span>Current Temperature: {temperature}°C</span>
+		</div>
+	);
+};
+
+const Content = ({ temperature }) => {
+	return (
+		<div>
+			<Temperature temperature={temperature} />
+		</div>
+	);
+};
+
+const Temperature = ({ temperature }) => {
+	return (
+		<>
+			<span>Current Temperature: {temperature}°C</span>
+		</>
+	);
+};
+
+function Footer() {
+	return (
+		<footer>
+			{/* Use on click to make it take action when we click  */}
+			{/* {} is to recall function from above in React */}
+			<button onClick={decreaseTemperature}>Up</button>
+			<button onClick={increaseTemperature}>Down</button>
+		</footer>
+	);
+}
 export default App;
