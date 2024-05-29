f826683b5c33dbd3d746d07e9d72010d4722f156
kanjitta <jittakan2539@gmail.com>
2024-05-29 12:39:35 +0700

    finish exercise 3

--- exercise_03/App.jsx
+++ exercise_03/App.jsx
@@ -1,9 +1,55 @@
 import React, { useState } from "react";
 
 const App = () => {
-	const temperature = 25;
+    const [temperature, setTemperature] = useState(25); // Initial temperature value
 
-	return <>{/* code here */};</>;
+    const decreaseTemperature = () => setTemperature((prevTemp) => prevTemp - 1);
+    const increaseTemperature = () => setTemperature((prevTemp) => prevTemp + 1);
+
+    return (
+        <>
+            <Header temperature={temperature} />;
+            <Content temperature={temperature} />;
+            <Footer temperature={temperature} />
+        </>
+    );
 };
 
+const Header = ({ temperature }) => {
+    return (
+        <div>
+            <button>ON / OFF</button>
+            <span>Current Temperature: {temperature}°C</span>
+        </div>
+    );
+};
+
+onst Content = ({ temperature }) => {
+	return (
+		<div>
+			<Temperature temperature={temperature} />
+		</div>
+	);
+};
+
+const Temperature = ({ temperature }) => {
+	return (
+		<>
+			<span>Current Temperature: {temperature}°C</span>
+		</>
+	);
+};
+
+function Footer() {
+	return (
+		<footer>
+			<button>Up</button>
+
+			<button>Down</button>
+		</footer>
+	);
+}
+
 export default App;
+
+
